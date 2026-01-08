cff7c5f0cbf39cce54915869ec19136b1bada1e3
ASUS <zatdat02@gmail.com>
2026-01-08 17:30:45 +0700

    spring be

---
+++ src/main/java/uth/edu/appchat/UthApplication.java
@@ -0,0 +1,13 @@
+package uth.edu.appchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uth/edu/appchat/UthApplicationTests.java
@@ -0,0 +1,13 @@
+package uth.edu.appchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
